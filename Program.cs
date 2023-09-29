8fe71a5eb6004ef79b587c51f093d17361a0ffe7
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-29 13:32:28 +0530

    [aditya]: the player then checks or a option. they are No play

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
-﻿namespace Player_rolls_UC2
+﻿namespace CheckForOption_UC3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("------------Snake and Ladder game-------");
-            SnakeLadder game = new SnakeLadder();
+            SnakLadder game = new SnakLadder();
             game.Board();
         }
     }
-    }
+    
+}
