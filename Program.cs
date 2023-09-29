34ffc085be2f9139e9a664e45b0e77383651c0de
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-29 12:54:36 +0530

    [aditya]: game played with single player at start position 0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Game_Start_UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------Snake and Ladder game-------");
+            SnakeLadder game=new SnakeLadder();
+            game.Board();
+        }
+    }
+}
---
+++ SnakeLadder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Start_UC1
+{
+    public class SnakeLadder
+    {
+        public void Start()
+        {
+            int position = 0;
+            int playerOne;
+
+            playerOne = position;
+            Console.WriteLine("player One position is {playerOne}");
+        }
+
+        public void Board()
+        {
+            Start();
+        }
+    }
+}
+           
+
+       
